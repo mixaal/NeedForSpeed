5c1609dd92543b3ade1f2f6ed1f5c67bcde7a551
Michal Conos <michal.conos@oracle.com>
2022-02-05 21:13:31 +0100

    Initial commit

---
+++ Assets/PlayerCarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarController : MonoBehaviour
+
+{
+    double dt = 0;
+    Vector3 last_position ;
+    bool collision;
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        collision = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(collision) {
+            transform.position = last_position;
+        }
+        last_position = transform.position;
+        Debug.Log(transform.position.y);
+        Debug.Log(collision);
+    }
+
+     private void OnTriggerEnter(Collider other)
+    {
+        collision = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Need for Speed
+
+The old 1995 Need for Speed  remake and _work in progress_ - this time in Unity 3d:
+
+![pic01](Doc/pic01.png)
+![pic02](Doc/pic02.png)
+![pic03](Doc/pic03.png)
+![pic04](Doc/pic04.png)
+
+
+
+
